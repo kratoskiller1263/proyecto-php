295751b24ef5be1526ca5370d986cc43fa1bbff2
kratoskiller1263 <142048594+kratoskiller1263@users.noreply.github.com>
2025-02-27 16:14:49 -0300

    Update UserRepositoryInterface.php

--- src/Domain/User/UserRepositoryInterface.php
+++ src/Domain/User/UserRepositoryInterface.php
@@ -1,9 +1,37 @@
 <?php
-namespace App\Domain\User;
-
 interface UserRepositoryInterface
 {
     public function save(User $user): void;
     public function findById(UserId $id): ?User;
     public function delete(UserId $id): void;
+}
+
+class DoctrineUserRepository implements UserRepositoryInterface
+{
+    private $entityManager;
+
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function save(User $user): void
+    {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+    public function findById(UserId $id): ?User
+    {
+        return $this->entityManager->find(User::class, $id->getId());
+    }
+
+    public function delete(UserId $id): void
+    {
+        $user = $this->findById($id);
+        if ($user) {
+            $this->entityManager->remove($user);
+            $this->entityManager->flush();
+        }
+    }
 }
